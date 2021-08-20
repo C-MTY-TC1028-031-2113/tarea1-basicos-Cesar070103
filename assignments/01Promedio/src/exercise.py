fb42d94a09e95bfea0ed19528822f83da8ce8dc7
César Benavides <a01285056@itesm.mx>
2021-08-20 18:47:39 -0500

    200821

--- assignments/01Promedio/src/exercise.py
+++ assignments/01Promedio/src/exercise.py
@@ -1,7 +1,18 @@
-def main():
-    #escribe tu código abajo de esta línea
-    pass
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+import os, sys
 
+#!/usr/bin/python
+# -*- coding: iso-8859-15 -*-
+import os, sys
+
+def main():
+    c1 = float(input("Calificación de la materia: "))
+    c2 = float(input("Calificación de la materia: "))
+    c3 = float(input("Calificación de la materia: "))
+    c4 = float(input("Calificación de la materia: "))
+    prom = float((c1+c2+c3+c4)/4)
+    print("El promedio es: " + str(prom))
 
 if __name__ == '__main__':
     main()
