a51db79e7cae2d94de05b7d584c6ceee0d818677
Cesar070103 <a01285056@itesm.mx>
2021-08-22 17:36:17 -0500

    220821

--- assignments/03PendienteLinea/src/exercise.py
+++ assignments/03PendienteLinea/src/exercise.py
@@ -1,9 +1,18 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+import os, sys
+
+#!/usr/bin/python
+# -*- coding: iso-8859-15 -*-
+import os, sys
+
 def main():
-    #escribe tu código abajo de esta línea
-    #Lee los datos
-    pass
-
-
+    x1 = float(input("Dame x1: "))
+    y1 = float(input("Dame y1: "))
+    x2 = float(input("Dame x2: "))
+    y2 = float(input("Dame y2: "))
+    pendiente = float((y2-y1)/(x2-x1))
+    print("Pendiente: " + str(pendiente))
 
 if __name__ == '__main__':
     main()
